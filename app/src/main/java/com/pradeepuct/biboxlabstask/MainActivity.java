0495381d9c354a8824e60c98a157f8aa0f73f235
pradeepinsta <pradeepravi24@gmail.com>
2023-06-16 14:47:26 +0530

    Initial commit

---
+++ app/src/main/java/com/pradeepuct/biboxlabstask/MainActivity.java
@@ -0,0 +1,28 @@
+package com.pradeepuct.biboxlabstask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,PartSelection.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/pradeepuct/biboxlabstask/PartSelection.java
@@ -0,0 +1,14 @@
+package com.pradeepuct.biboxlabstask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PartSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part_selection);
+    }
+}
